76394f5d0fc5f649737390369a30505cfbd7e04a
karthickmurugan2911 <karthick.murugan02@gmail.com>
2025-07-30 17:19:11 +0530

    Phpcbf update
    Phpcbf update

--- comments.php
+++ comments.php
@@ -4,125 +4,166 @@
 
 class sampleclass
 {
-
-function __construct(){
-    $this->message= "Welcome to the PHPCS test!";
-}
-
-	
-function hello_world( $user ){
-echo "Hello ".esc_html($user)."!";
-}
-
-public function add($a,$b){return$a+$b;}
-
-
-function veryLongFunctionNameWithNoSpaces($first,$second ,$third ,$fourth  ,$fifth){
-$result=$first+$second+$third+$fourth+$fifth;
-return$result;
-}
-
-
-
-    public function loopTest(){
-for ( $i=0;$i<10;$i++ ){
-echo esc_html($i)."\n";
-}
+    function __construct()
+    {
+        $this->message = "Welcome to the PHPCS test!";
     }
 
 
-function noVisibilitySet(){
-    $x=  5;   $y=10;return$x+$y;
-}
-
-
-function spacingIssue(  $var1,$var2){
-return $var1+ $var2 ;
-}
-
-
-function badNaming1($VAL){return$VAL;}
-function BAD_function($Z){ return $Z * 2;}
-
-function TabsVsSpaces(){
-	$tabbed = "this line uses tabbing";
-    $spaced    = "this line uses spaces updated";
-	echo esc_html($tabbed);
-echo esc_html($spaced);
-}
-
-function NoDocCommentFunction(){
-    return true;
-}
-
-function inlinecontrol(){ if(true){ echo "yes";}else{echo "no";}}
-
-function logicOutput($input){
-    if($input > 5){
-        echo "Big!";
-    } else {
-        echo "Small!";
+    function hello_world($user)
+    {
+        echo "Hello " . esc_html($user) . "!";
     }
-}
+
+    public function add($a, $b)
+    {
+        return$a + $b;
+    }
 
 
-function MissingBraces($val){
-if($val === 1) {
-echo "Single line";
-}
-}   
-
-function LongArray(){
-$arr = array( 1 , 2 ,3 ,4,5  );
-return $arr;
-}
+    function veryLongFunctionNameWithNoSpaces($first, $second, $third, $fourth, $fifth)
+    {
+        $result = $first + $second + $third + $fourth + $fifth;
+        return$result;
+    }
 
 
 
-function inlineHTML(){
-    ?><div class="warning">This is bad practice</div><?php
-}
-
-function     multipleSpaces(  $a ,  $b  ){
-    return $a    +     $b;
-}
-
-function noTypeHints($x, $y){
-return$x * $y;
-}
+    public function loopTest()
+    {
+        for ($i = 0; $i < 10; $i++) {
+            echo esc_html($i) . "\n";
+        }
+    }
 
 
-function messySwitch($fruit){
-switch($fruit){
-case 'apple': echo 'Red';break;
-case 'banana':echo'Yellow';break;
-default:echo 'Unknown';break;
-}
-}
+    function noVisibilitySet()
+    {
+        $x =  5;
+        $y = 10;
+        return$x + $y;
+    }
 
 
-function snake_case_example($input_string){
-return strtoupper($input_string);
-}
+    function spacingIssue($var1, $var2)
+    {
+        return $var1 + $var2 ;
+    }
 
 
-function capitalLetters($BADINPUT){
-RETURN strtolower($BADINPUT);
-}
+    function badNaming1($VAL)
+    {
+        return$VAL;
+    }
+    function BAD_function($Z)
+    {
+        return $Z * 2;
+    }
 
-function NotCamelCase(){
-$MyVar = 10;
-return $MyVar;
-}
+    function TabsVsSpaces()
+    {
+        $tabbed = "this line uses tabbing";
+        $spaced    = "this line uses spaces updated";
+        echo esc_html($tabbed);
+        echo esc_html($spaced);
+    }
+
+    function NoDocCommentFunction()
+    {
+        return true;
+    }
+
+    function inlinecontrol()
+    {
+        if (true) {
+            echo "yes";
+        } else {
+            echo "no";
+        }
+    }
+
+    function logicOutput($input)
+    {
+        if ($input > 5) {
+            echo "Big!";
+        } else {
+            echo "Small!";
+        }
+    }
 
 
-function endsWithSpace() 
-{ 
-    return true; 
-}    
+    function MissingBraces($val)
+    {
+        if ($val === 1) {
+            echo "Single line";
+        }
+    }
 
-function endsWithExtraLines(){
-    return true;
-}
+    function LongArray()
+    {
+        $arr = array( 1 , 2 ,3 ,4,5  );
+        return $arr;
+    }
 
+
+
+    function inlineHTML()
+    {
+        ?><div class="warning">This is bad practice</div><?php
+    }
+
+    function multipleSpaces($a, $b)
+    {
+        return $a    +     $b;
+    }
+
+    function noTypeHints($x, $y)
+    {
+        return$x * $y;
+    }
+
+
+    function messySwitch($fruit)
+    {
+        switch ($fruit) {
+            case 'apple':
+                echo 'Red';
+                break;
+            case 'banana':
+                echo'Yellow';
+                break;
+            default:
+                echo 'Unknown';
+                break;
+        }
+    }
+
+
+    function snake_case_example($input_string)
+    {
+        return strtoupper($input_string);
+    }
+
+
+    function capitalLetters($BADINPUT)
+    {
+        return strtolower($BADINPUT);
+    }
+
+    function NotCamelCase()
+    {
+        $MyVar = 10;
+        return $MyVar;
+    }
+
+
+    function endsWithSpace()
+    {
+        return true;
+    }
+
+    function endsWithExtraLines()
+    {
+        return true;
+    }
 }
